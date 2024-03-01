c55b6933cb0840c79c13e2a62ec8a2a00a1a8b0d
Thai1708 <88660353+Thai1708@users.noreply.github.com>
2024-03-02 03:07:19 +0700

    Delete recruitment directory

--- recruitment/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-
-import os
-from time import sleep
-import random
